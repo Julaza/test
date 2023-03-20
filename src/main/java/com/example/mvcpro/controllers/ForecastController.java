642ed70e87eafdd93d53ae8b734fc22236f03427
julia <julazac@gmail.com>
2023-03-20 09:53:32 +0200

    inprogress

--- src/main/java/com/example/mvcpro/controllers/ForecastController.java
+++ src/main/java/com/example/mvcpro/controllers/ForecastController.java
@@ -12,25 +12,32 @@ public class ForecastController {
     @GetMapping("/")
     public ModelAndView index(){
         var modelAndView = new ModelAndView("index");
-
         var indexModel = new IndexModel();
 
-        var cities = new ArrayList<String>();
-        cities.add("Vilnius");
-        cities.add("Kaunas");
+        ArrayList<String> cities = getCities();
         indexModel.cities = cities;
-//
 
-        var forecasts = new ArrayList<ForecastModel>();
-        forecasts.add(new ForecastModel("2023-02-20" , 1));
-        forecasts.add(new ForecastModel("2023-01-01" , 2));
-        forecasts.add(new ForecastModel("2023-03-03" , 3));
+        ArrayList<ForecastModel> forecasts = getForecast();
         indexModel.forecasts = forecasts;
-//
 
         modelAndView.addObject("IndexModel", indexModel);
 
         return modelAndView;
+    }
+
+    private static ArrayList<String> getCities() {
+        var cities = new ArrayList<String>();
+        cities.add("Vilnius");
+        cities.add("Kaunas");
+        return cities;
+    }
+
+    private static ArrayList<ForecastModel> getForecast() {
+        var forecasts = new ArrayList<ForecastModel>();
+        forecasts.add(new ForecastModel("2023-02-20" , 1));
+        forecasts.add(new ForecastModel("2023-01-01" , 2));
+        forecasts.add(new ForecastModel("2023-03-03" , 3));
+        return forecasts;
     }
 
 }
