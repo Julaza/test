f6137540fdba67b0454581650a0b0c4072c33e68
SMK\210-4 <julazac@gmail.com>
2023-03-20 17:24:54 +0200

    ajax

--- src/main/java/com/example/mvcpro/controllers/ForecastRestController.java
+++ src/main/java/com/example/mvcpro/controllers/ForecastRestController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class ForecastRestController { // priima plikus duomenis, bet neatvaizduoja jokiu view/html, dirba tik su duomenim
-
+//nk nedaro, bet perduos i duomenu baze
     @PostMapping (value = "/api/forecast", consumes = "application/json") // siusim i serveri ka norim issaugot
     public void index(@RequestBody ForecastModel model) {
 
