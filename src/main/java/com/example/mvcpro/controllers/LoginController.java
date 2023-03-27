2407c7fb248813128f559f11b865d5cf17191d76
SMK\210-4 <julazac@gmail.com>
2023-03-27 08:54:27 +0300

    basic hardcoded autification

---
+++ src/main/java/com/example/mvcpro/controllers/LoginController.java
@@ -0,0 +1,18 @@
+package com.example.mvcpro.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.io.IOException;
+
+@Controller
+public class LoginController {
+    @GetMapping("/login")
+    public ModelAndView login() {
+
+        ModelAndView modelAndView = new ModelAndView("login");
+        return modelAndView;
+    }
+}
